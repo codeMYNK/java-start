9f9eced6fd55acc5e3720df81c5f423fe2c95285
Mayank <mynk8076@gmail.com>
2024-09-12 14:33:05 +0530

    Input_taking, Sum

--- Input_taking.java
+++ Input_taking.java
@@ -3,5 +3,11 @@ import java.util.*;
 public class Input_taking {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Name");
+        String name = sc.nextLine();
+        //nextInt()
+        //nextFloat()
+        //nextDouble() ....and many more
+        System.out.println("name: " + name);
     }
 }
---
+++ Sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Number 1");
+        int Num1 = sc.nextInt();
+
+        System.out.println("Number 2");
+        int Num2 = sc.nextInt();
+
+        int result = Num1 + Num2;
+        System.out.println("result: " + result);
+    } 
+}
