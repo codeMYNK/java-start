867383f45ec0d15b5f0446fcac745e653b749b3d
Mayank <mynk8076@gmail.com>
2024-09-12 14:15:58 +0530

    dtaya_types, Input Taking

---
+++ data_types.java
@@ -0,0 +1,22 @@
+public class data_types {
+    public static void main(String[] args) {
+        //variables
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println("sum:" +sum);
+
+        int diff = b - a;
+        System.err.println("diffrence:" +diff);
+
+        int multiply = b - a;
+        System.out.println("multiply:" +multiply);
+
+        //To Calculate 
+        // a*b / a+b
+
+        int ans = a*b / a-b;
+        System.out.println("result:" + ans);
+
+    }
+}
